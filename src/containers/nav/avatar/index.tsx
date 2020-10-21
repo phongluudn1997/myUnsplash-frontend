17166c94fb2c30114ad74b4c9eb847121558b516
Phong Luu <luu.huynh@fiisoft.com>
2020-10-21 17:43:38 +0700

    #wip: useClickOutside

--- src/containers/nav/avatar/index.tsx
+++ src/containers/nav/avatar/index.tsx
@@ -1,25 +1,36 @@
-import React from "react";
+import React, { SyntheticEvent, useState } from "react";
 import styles from "./styles.module.scss";
 import { Redirect, withRouter } from "react-router";
 import { useAuth } from "context/auth";
 
 const Avatar = (props: any) => {
   const { setAuthToken } = useAuth();
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
 
   const handleClick = () => {
     props.history.push("/login");
     setAuthToken("");
   };
+
+  const handleClickDropdown = (e: SyntheticEvent) => {
+    setDropdownVisible((isDropdownVisible) => !isDropdownVisible);
+  };
+
   return (
     <div className={styles["dropdown-container"]}>
-      <img
-        src="https://www.kindpng.com/picc/m/22-223941_transparent-avatar-png-male-avatar-icon-transparent-png.png"
-        alt="Avatar"
-        className="rounded-full h-10"
-      />
-      <div className={styles["dropdown-content"]}>
-        <button onClick={handleClick}>Log out</button>
-      </div>
+      <button>
+        <img
+          onClick={handleClickDropdown}
+          src="https://www.kindpng.com/picc/m/22-223941_transparent-avatar-png-male-avatar-icon-transparent-png.png"
+          alt="Avatar"
+          className="rounded-full h-10"
+        />
+      </button>
+      {isDropdownVisible && (
+        <div className={styles["dropdown-content"]}>
+          <button onClick={handleClick}>Log out</button>
+        </div>
+      )}
     </div>
   );
 };
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 import useFetchData from "./use-fetch-data";
+import useClickOutside from "./use-click-outside";
 
-export { useFetchData };
+export { useFetchData, useClickOutside };
