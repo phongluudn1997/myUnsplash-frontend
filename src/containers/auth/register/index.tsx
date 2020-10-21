192ff94089a2013f023219473ab690a232f161e1
Phong Luu <luu.huynh@fiisoft.com>
2020-10-21 11:07:42 +0700

    registerPage

--- src/containers/auth/register/index.tsx
+++ src/containers/auth/register/index.tsx
@@ -3,24 +3,24 @@ import { useFetchData } from "../../../hooks/";
 import { DataProfile } from "../../../data-access";
 import { useAuth } from "../../../context/auth";
 import { Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 type Props = any;
 
 const Register: React.FC<Props> = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
   const [isLoggedIn, setLoggedIn] = useState(false);
-  const { setAuthToken } = useAuth();
   const referer = props.location?.state?.referer || "/";
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     try {
-      const response = await DataProfile.Post("/user/login", {
-        data: { email, password },
+      const response = await DataProfile.Post("/user/register", {
+        data: { email, password, nickname },
       });
-      setAuthToken(`Bearer ${response.data.data.token}`);
-      setLoggedIn(true);
+      props.history.push("/login");
     } catch (err) {
       console.log(err);
     }
@@ -49,6 +49,23 @@ const Register: React.FC<Props> = (props) => {
             type="text"
             placeholder="Email"
             onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="nickname"
+          >
+            Nickname
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="nickname"
+            type="text"
+            placeholder="Nickname"
+            onChange={(e) => setNickname(e.target.value)}
+            required
           />
         </div>
         <div className="mb-6">
@@ -65,6 +82,7 @@ const Register: React.FC<Props> = (props) => {
             id="password"
             type="password"
             placeholder="******************"
+            required
           />
         </div>
         <div className="flex items-center justify-between">
@@ -72,14 +90,14 @@ const Register: React.FC<Props> = (props) => {
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit"
           >
-            Sign In
+            Sign up
           </button>
-          <a
+          <Link
             className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            href="#"
+            to="/login"
           >
-            Forgot Password?
-          </a>
+            Already has account?
+          </Link>
         </div>
       </form>
       <p className="text-center text-gray-500 text-xs">
