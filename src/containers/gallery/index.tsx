3645b30ee8b971711572e08a8c4e72e759d23292
Phong Luu <luu.huynh@fiisoft.com>
2020-10-26 09:05:47 +0700

    update library

--- src/containers/gallery/index.tsx
+++ src/containers/gallery/index.tsx
@@ -4,10 +4,10 @@ import Gallery from "react-photo-gallery";
 import demoPhotos from "./demoPhotos";
 import { DataProfile } from "data-access";
 import "./gallery.scss";
+import Masonry from "react-masonry-css";
 
 export default (props: any) => {
   const [statePhotos, setPhotos] = useState<any[]>([]);
-  console.log(props);
 
   useEffect(() => {
     fetchData();
@@ -16,59 +16,24 @@ export default (props: any) => {
   const fetchData = async function () {
     const res = await DataProfile.Get("/photos");
     const { photos } = res.data.data;
-
-    const finalPhotos = photos
-      .filter((photo: any) => {
-        return photo.url;
-      })
-      .map((photo: any) => {
-        return {
-          src: photo.url,
-          height: Math.floor(Math.random() * 4) + 1,
-          width: 4,
-        };
-      });
-
-    console.log(finalPhotos);
-    setPhotos(finalPhotos);
+    setPhotos([...photos, ...statePhotos]);
   };
 
-  function calculateSection() {
-    return statePhotos.map((photo, i) => {
-      return (
-        <section className="gallery">
-          {statePhotos.slice(i * 14, i * 14 + 14).map((photo, index) => (
-            <figure className={`gallery__item gallery__item--${index + 1}`}>
-              <img
-                src={photo.src}
-                alt="Gallery image 1"
-                className="gallery__img"
-              />
-            </figure>
-          ))}
-        </section>
-      );
-    });
-  }
-
-  if (!statePhotos.length) {
-    return <div>Loading</div>;
+  console.log(statePhotos);
+  if (!statePhotos) {
+    return "Loading...";
   }
   return (
-    <div>{calculateSection()}</div>
-
-    // <section className="gallery">
-    //   {statePhotos.map((photo, index) => {
-    //     return (
-    //       <figure className={`gallery__item gallery__item--${index + 1}`}>
-    //         <img
-    //           src={photo.src}
-    //           alt="Gallery image 1"
-    //           className="gallery__img"
-    //         />
-    //       </figure>
-    //     );
-    //   })}
-    // </section>
+    <Masonry
+      breakpointCols={3}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      {statePhotos.map((photo) => (
+        <div style={{ borderRadius: 12, overflow: "hidden" }}>
+          <img src={photo.url} alt="photo" />
+        </div>
+      ))}
+    </Masonry>
   );
 };
